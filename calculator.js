7fc8ebbe37d2995066ab04832ee97721bc18e8d5
lolj321 <43753022+lolj321@users.noreply.github.com>
2019-01-26 04:40:31 -0500

    did the best I could

--- calculator.js
+++ calculator.js
@@ -1,10 +1,10 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand : false,
-  operator: null,
-};
-
+var  expressionArray = [];
+let currentValue = "0";
+let valueOne;
+let valueTwo;
+var numDecimalInsert = false;
+var placeInsert = false;
+var exponentialAlert = false;
 // Maximum length of characters
 // to display for result.
 const MAX_RESULT_LENGTH = 8;
@@ -15,7 +15,7 @@ const MAX_NUMBER = 9999999;
 
 function calculatorStartup() {
   // to set screen to clear instead of adding 0's
-  document.getElementById("screen").value += 0;
+  document.getElementById("screen").value += currentValue;
 }
 
 function updateDisplay() {
@@ -25,11 +25,13 @@ function updateDisplay() {
 }
 
 function pi() {
-  document.getElementById("screen").value = 3.14159265359;
+  document.getElementById("screen").value += "3.14159265359";
 }
 
-function numb(numb)  {
-
+function number(numb)  {
+  if (screen.value === "0") {
+    screen.value = "";
+  }
 // To check if numbers appear in console
 if (document.getElementById("screen").value != "0") {
   console.log("1");
@@ -41,92 +43,114 @@ else if (document.getElementById("screen").value == "0") {
   document.getElementById("screen").value = numb;
   console.log("2");
 }
-
-
-
-
-/*  const { displayValue, waitingForSecondOperand } = calculator;
-
-  if (waitingForSecondOperand === true) {
-  calculator.displayValue = number;
-  calculator.waitingForSecondOperand = false;
-} else {
-  displayValue === '0' ? number : displayValue + number;
-} */
+document.getElementById("number(numb)");
 }
 
-function negate(numb) {
-  // console.log(numb);
-  // console.log(typeof numb);
-  numb = Number(numb);
-  // 
-  
-  if (document.getElementById("screen").value != "0") {
-  numb = negate;
-  }  else {
-    document.getElementById("screen").value = 0
-    }
-    console.log("negate");
-    document.getElementById("screen").value = negate ; 
+function negate() {
+  document.getElementById("screen").value = document.getElementById("screen").value * -1;
 }
 function percent() {
-
+    document.getElementById("screen").value = document.getElementById("screen").value / 100;
 }
 
 function decimal() {
-  /* if (calculator.waitingForSecondOperand  === true) return;
-
-    if (!calculator.displayValue.includes(dot)) {
-      calculator.displayValue += dot;
-    }*/
   document.getElementById("screen").value += ".";
   console.log(5);
 }
 
 function clearScreen() {
-  document.getElementById("screen").value = 0;
+  document.getElementById("screen").value = "0";
+  document.getElementById("baby-screen").value = "";
 }
 
-function handleOperator() {
-  document.getElementById("baby-screen").value += (calculator.firstOperand + calculator.operator);
-  document.getElementById("screen").value = displayValue;
+ function divide() {
+  let valueOne = document.getElementById("screen").value;
+  document.getElementById("baby-screen").value = valueOne;
+  document.getElementById("screen").value += " / ";
+  document.getElementById("baby-screen").value += " / "
+} 
 
-  const { firstOperand, displayValue, operator } = calculator
-  const inputValue = parseFloat(displayValue);
-
-  if (operator && calculator.waitingForSecondOperand)  {
-    calculator.operator = nextOperator;
-    return;
-  }
-
-  if (firstOperand == null) {
-    calculator.firstOperand = inputValue;
-  } else if (operator) {
-    const currentValue = firstOperand || 0;
-    const result = performCalculation[operator](currentValue, inputValue);
-
-    calculator.displayValue = String(result);
-    calculator.firstOperand = result;
-  }
-
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
+function plus() {
+  let valueOne = document.getElementById("screen").value;
+  document.getElementById("baby-screen").value = valueOne;
+  document.getElementById("screen").value += " + ";
+  document.getElementById("baby-screen").value += " + "
 }
 
-const performCalculation = {
-  '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
 
-  '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+function minus() {
+  let valueOne = document.getElementById("screen").value;
+  document.getElementById("baby-screen").value = valueOne;
+  document.getElementById("screen").value += " - ";
+  document.getElementById("baby-screen").value += " - "
+}
 
-  '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
 
-  '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+function times() {
+  let valueOne = document.getElementById("screen").value;
+  document.getElementById("baby-screen").value = valueOne;
+  document.getElementById("screen").value += " * ";
+  document.getElementById("baby-screen").value += " * ";
 
-  '=': (firstOperand, secondOperand) => secondOperand,
-  '%': (firstOperand, secondOperand) => firstOperand / 100,
-};
+}
+
+function commasPlace() {
+  if (placeInsert == false) {
+    document.calc.display.value = document.calc.display.value + numb;
+    expressionArray.push(numb)
+    console.log("in array" , expressionArray.join(''))
+  }
+  
+  if(Number(document.calc.display.value.length) <= 10) {
+    document.calc.display.value
+  } else {
+    document.getElementById("numbers(numb)").disabled = true;
+    document.getElementById("dot").disabled = true;
+    
+    if(decimalNumInsert == false){
+      let commaInput = Number(document.getElementById("screen").value.split(",").join("")).toLocaleString;
+      document.getElementById("screen").value = commaInput;
+      document.getElementById("baby-screen").value = commaInput;
+      
+    }
+  }
+}
 
 function equal() {
-  document.getElementById("equal-sign").value == eval(expression.toString(numb));
-  console.log(numb + numb);
+  result = document.getElementById("screen").value;
+  document.getElementById("screen").value = eval(document.getElementById("screen").value.toLocaleString("en"));
+  console.log(result.toLocaleString("en"));
+  notation();
+  commasPlace();
+  
+}
+
+function notation() {
+  let numExpress = eval(expressionArray.join(''));
+  if (expressionArray.join(",").includes("e")) {
+    exponentialAlert = true;
+    console.log(expressionArray)
+    
+  }
+    expressionArray = [];
+    expressionArray.push(numExpress);
+    console.log("evaluated raw", numExpress);
+  if (document.getElementById("screen").value > 999999999 || document.getElementById("screen").value < 999999999) {
+    document.getElementById("screen").value = numb.length(9)
+  } else {
+    document.getElementById("screen").value = document.getElementById("screen").value.toLocaleString("en")
+  }
+  if(exponentialAlert == true) {
+    document.getElementById("screen").value = numExpress;
+  }
+  
+}
+
+function error() {
+  if (numb === Infinity || numb === NaN || numb === undefined) {
+    numb = "Infinity";
+  }
+}
+function rootx() {
+  document.getElementById("screen").value = Math.sqrt((Number((document.getElementById("screen").value))));
 }
